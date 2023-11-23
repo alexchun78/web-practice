b3d79522175a50640f106124be65e25dea70b976
alexchun <alexchun78@gmail.com>
2023-11-23 18:00:55 +0900

    scroll 관련 내용

--- js/main.js
+++ js/main.js
@@ -2,15 +2,37 @@ const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
 searchEl.addEventListener('click', function(){
-    searchInputEl.focus();
+  searchInputEl.focus();
 });
 
 searchInputEl.addEventListener('focus', function(){
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder','통합검색');
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder','통합검색');
 });
 
 searchInputEl.addEventListener('blur', function(){
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder','');
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder','');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+// loadash의 throttle 기능으로 많은 횟수로 실행되는 함수를 제어한다.
+// _.throttle(함수, 시간)
+window.addEventListener('scroll', _.throttle(function(){
+  if(window.scrollY > 500){
+    // 배지 숨기기
+    //gasp.to(요소, 지속시간, 옵션)
+    gsap.to(badgeEl, 0.6, {
+      opacity:0,
+      display : 'none'
+    });
+  } else {
+    
+    gsap.to(badgeEl, 0.6, {
+      opacity:1,
+      display : 'block'
+    });
+  }
+},300
+));
