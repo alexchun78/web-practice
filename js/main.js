f1775658d640452b98db51ea75c5c6922e994d8d
alexchun78 <alexchun78@gmail.com>
2023-11-25 07:36:30 +0900

    notice 슬라이드(swipeJS) 적용 완료

--- js/main.js
+++ js/main.js
@@ -1,47 +1,55 @@
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
-searchEl.addEventListener('click', function(){
+searchEl.addEventListener('click', function () {
   searchInputEl.focus();
 });
 
-searchInputEl.addEventListener('focus', function(){
+searchInputEl.addEventListener('focus', function () {
   searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder','통합검색');
+  searchInputEl.setAttribute('placeholder', '통합검색');
 });
 
-searchInputEl.addEventListener('blur', function(){
+searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder','');
+  searchInputEl.setAttribute('placeholder', '');
 });
 
 const badgeEl = document.querySelector('header .badges');
 
 // loadash의 throttle 기능으로 많은 횟수로 실행되는 함수를 제어한다.
 // _.throttle(함수, 시간)
-window.addEventListener('scroll', _.throttle(function(){
-    if(window.scrollY > 500){
-      // 배지 숨기기
-      //gasp.to(요소, 지속시간, 옵션)
-      gsap.to(badgeEl, 0.6, {
-        opacity:0,
-        display : 'none'
-      });
-    } else {
-      
-      gsap.to(badgeEl, 0.6, {
-        opacity:1,
-        display : 'block'
-      });
-    }
-  },300
+window.addEventListener('scroll', _.throttle(function () {
+  if (window.scrollY > 500) {
+    // 배지 숨기기
+    //gasp.to(요소, 지속시간, 옵션)
+    gsap.to(badgeEl, 0.6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+
+    gsap.to(badgeEl, 0.6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300
 ));
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
-fadeEls.forEach(function(fadeEl,index){
+fadeEls.forEach(function (fadeEl, index) {
   //gasp.to(요소, 지속시간, 옵션)
-  gsap.to(fadeEl,1, {
-    delay: (index+1)*0.7, // 0.7, 1.4, 2.1, 2.7초로 순서대로 애니메이션 실행
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7, // 0.7, 1.4, 2.1, 2.7초로 순서대로 애니메이션 실행
     opacity: 1
   });
+});
+
+
+const swiper = new Swiper('.notice-line .swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  autoplay: true,
+  loop: true,
 });
