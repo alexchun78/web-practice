501694ecf67baa7230bd0c2e24d2bd2689585e44
alexchun78 <alexchun78@gmail.com>
2023-11-25 15:37:03 +0900

    promotion slide 작업 완료

--- js/main.js
+++ js/main.js
@@ -47,9 +47,28 @@ fadeEls.forEach(function (fadeEl, index) {
 });
 
 
-const swiper = new Swiper('.notice-line .swiper', {
+new Swiper('.notice-line .swiper', {
   // Optional parameters
   direction: 'vertical',
   autoplay: true,
   loop: true,
+});
+
+new Swiper('.promotion .swiper', {
+  // Optional parameters
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, //1번 슬라이드가 가운데 보이기
+  loop: true,
+  // autoplay: {
+  //   delay: 500,
+  // },
+  pagination: {
+    el: '.promotion .swiper-pagination', //페이지 번호 요소 선택자
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.promotion .swiper-button-next',
+    prevEl: '.promotion .swiper-button-prev',
+  }
 });
